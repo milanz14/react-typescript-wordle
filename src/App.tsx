acdf374994633b3f75013c6056c3d7a0aa32cf47
Milan Zagorac <zagoracm@gmail.com>
2022-09-17 17:54:56 -0400

    done

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { words } from "./data/words";
 import GuessLine from "./components/GuessLine";
 
 function App(): JSX.Element {
-  const [gameWord, setGameWord] = useState("");
-  const [guesses, setGuesses] = useState(Array(6).fill(null));
+  const [gameWord, setGameWord] = useState<string>("");
+  const [guesses, setGuesses] = useState(Array(6).fill(""));
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+  const [count, setCount] = useState<number>(0);
 
   const guessInputRef = useRef<HTMLInputElement>(null);
   const gameWordRef = useRef<string | null>(null);
-  const guessedWordRef = useRef<string | null>(null);
+  const guessedWordRef = useRef<string>("");
 
   // save a random word on app mount
   useEffect(() => {
@@ -28,33 +30,71 @@ function App(): JSX.Element {
     } else if (wordForGuess.length !== 6) {
       alert("Must be 6 letter word.");
     } else if (words.indexOf(wordForGuess) === -1) {
-      alert("Not a valid 6 letter word option.");
+      return;
     } else {
       guessedWordRef.current = wordForGuess;
+      checkGuessedWord();
     }
-    checkGuessedWord();
     guessInputRef.current!.value = "";
   };
 
-  const checkGuessedWord = () => {
+  const checkGuessedWord = (): void => {
     if (guessedWordRef.current === gameWordRef.current) {
-      alert("WINNER");
+      handleGuesses();
+      setIsGameOver(true);
+      return;
     }
+    handleGuesses();
+  };
+
+  const handleGuesses = (): void => {
+    setCount((count) => count + 1);
+    const newGuesses = [...guesses];
+    newGuesses[guesses.findIndex((val) => val === "")] = guessedWordRef.current;
+    setGuesses(newGuesses);
+  };
+
+  const handleGameReset = () => {
+    guessedWordRef.current = "";
+    setGuesses(Array(6).fill(""));
+    setIsGameOver(false);
+    setCount(0);
   };
 
   return (
     <div className="App">
       <h2>6Wordle</h2>
-      {gameWord}
       <form className="guessesInput" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Input guess... " ref={guessInputRef} />
-        <button type="submit">Check Guess</button>
+        <input
+          type="text"
+          placeholder="Input guess... "
+          ref={guessInputRef}
+          disabled={isGameOver}
+        />
+        <button type="submit" disabled={isGameOver}>
+          Check Guess
+        </button>
       </form>
       <div className="gameboard">
         {guesses.map((guess: string, idx) => {
-          return <GuessLine guess={guess ?? ""} key={idx} />;
+          const isCurrentGuess =
+            idx === guesses.findIndex((val) => val === null);
+          return (
+            <GuessLine
+              guess={isCurrentGuess ? guessedWordRef.current : guess ?? ""}
+              isFinal={!isCurrentGuess && guess !== ""}
+              solution={gameWord}
+              key={idx}
+            />
+          );
         })}
       </div>
+      {isGameOver && (
+        <div>
+          <p>It took you {count} tries!</p>
+          <button onClick={handleGameReset}>Reset</button>
+        </div>
+      )}
     </div>
   );
 }
