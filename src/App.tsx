34b0854a1b299361fe38321d8fecfe608ee49000
Milan Zagorac <zagoracm@gmail.com>
2022-09-17 09:09:21 -0400

    remove strict mode which causes two useEffect renders in development mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
 
+import {words} from "./data/words"
+
 function App() {
+
+  useEffect(() => {
+    const randIdx = Math.floor(Math.random() * words.length)
+    const randWord = words[randIdx]
+    console.log(randWord)
+
+  },[])
+
   return (
     <div className="App">
      <h1>Placeholder</h1>
