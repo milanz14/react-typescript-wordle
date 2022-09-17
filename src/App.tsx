b15e7c2e1ef76488e786e726feb7422a30cb2bab
Milan Zagorac <zagoracm@gmail.com>
2022-09-17 10:44:02 -0400

    render a line for each guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { words } from "./data/words";
 
-import './App.css';
-
-import {words} from "./data/words"
+import GuessLine from "./components/GuessLine";
 
 function App() {
+  const [gameWord, setGameWord] = useState("");
+  const [guessedWord, setGuessedWord] = useState("");
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
 
-  const [ gameWord, setGameWord ] = useState('')
-
+  // save a random word on app mount
   useEffect(() => {
-    const randIdx = Math.floor(Math.random() * words.length)
-    const randWord = words[randIdx]
-    setGameWord(randWord)
-  },[])
+    const randIdx = Math.floor(Math.random() * words.length);
+    const randWord = words[randIdx].toUpperCase();
+    setGameWord(randWord);
+  }, []);
 
   return (
     <div className="App">
-     {gameWord}
+      {gameWord}
+      <div className="guessesInput">
+        <input type="text" placeholder="Input guess... " />
+        <button>Check Guess</button>
+      </div>
+      <div className="gameboard">
+        {guesses.map((guess) => {
+          return <GuessLine guess={guess} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/GuessLine.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WORD_LENGTH = 6;
+
+interface GuessLineProps {
+  guess: string;
+}
+
+const GuessLine = ({ guess }: GuessLineProps) => {
+  const squares = [];
+  return <h2>Test</h2>;
+};
+
+export default GuessLine;
