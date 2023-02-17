2a8ada97fe842d65d1a5eca00ec9e1afa6391328
Milan Zagorac <zagoracm@gmail.com>
2023-02-17 11:25:08 -0500

    install Chakra, set up Chakra provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 // remove strict mode to prevent useEffect running twice in <App />
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
